d7d645caa3ad0262da91ebfcb362a2329d43cdf6
YKizou <62426491+YKizou@users.noreply.github.com>
2022-03-15 11:12:53 +0100

    Change screenshot template

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import About from "./About";
 import Contact from "./Contact";
 import LeftBar from "./LeftBar";
